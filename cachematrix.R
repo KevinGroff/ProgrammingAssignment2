39ac8a45864d6ba12fd3a07c3390f264b2aee93b
Kevin Groff <kevin.groff@gmail.com>
2015-09-24 13:02:45 -0400

    GroffK Attempt 1
    First attempt at completing the HW

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix 
+##rather than compute it repeatedly. These functions cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <- NULL
+  
+  ## Replace active matrix with new matrix
+  SetActiveMatrix <- function(y)
+  {
+    ## Set new matrix as active matrix
+    x <<- y
+    
+    ## Since active matrix is new, cached value is no longer valid to must be SetActiveMatrix to null
+    m <<- NULL
+  }
+  
+  ## Return value of active matrix
+  GetActiveMatrix <- function()
+  {
+    x
+  }
+  
+  ## Save value of inverse matrix to cache
+  SaveCacheMatrix<- function(solve)
+  {
+    m <<- solve(x)
+  }
+  
+  ## Return value of cached matrix
+  ReadCacheMatrix <- function()
+  {
+    m
+  }
+  
+  
+  list	(SetActiveMatrix = SetActiveMatrix,
+        GetActiveMatrix = GetActiveMatrix,
+        SaveCacheMatrix = SaveCacheMatrix,
+        ReadCacheMatrix = ReadCacheMatrix
+  )
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve will retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) 
+{
+  
+  ## SetActiveMatrix m to the current stored matrix
+  m <- x$ReadCacheMatrix()
+  
+  print(m)
+  ## Check if m has a cached matrix.  If yes, then returned the cached value.
+  if(!is.null(m)) 
+  {
+    # Return value of cached matrix and notify with message
+    message("getting cached matrix")
+    return(m)
+  }
+  
+  ## If matrix is NULL, then the inverse must be calculated.
+  m <- solve(x$GetActiveMatrix())
+  
+  ## Cache the inversed matix
+  x$SaveCacheMatrix(m)
+  
+  ## Return the inverse matrix
+  m
+}  
+
